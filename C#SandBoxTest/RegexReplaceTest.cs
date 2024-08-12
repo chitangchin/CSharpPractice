97736877482e47674cc9c66d8d57e2a2ab28d3ee
chitangchin <chitangb.chin@gmail.com>
2024-08-12 11:24:42 -0400

    Practicing Regex Replace Method

--- C#SandBoxTest/RegexReplaceTest.cs
+++ C#SandBoxTest/RegexReplaceTest.cs
@@ -3,5 +3,10 @@
     using Sandbox.Regex;
     public class RegexReplaceTests
     {
+        public void Tests()
+        {
+            string test = "1231312@!#!@#!@asdasfa@!#!@%!@%!#^";
+            Assert.That(SolutionClass.RegexReplace(test), Is.EqualTo("1231312asdasfa"));
+        }
     }
 }
--- C#Sandbox/RegexReplace.cs
+++ C#Sandbox/RegexReplace.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*   
+Objective: Remove special characters
+*/
 
 namespace Sandbox.Regex
 {
+    //Namespace for the Regex Class
+    using System.Text.RegularExpressions;
     public class SolutionClass
     {
-        public string RegexReplace(string arg)
+        public static string RegexReplace(string arg)
         {
-            return "Test";
+            //Create a pattern to hold all alphanumeric characters
+            string pattern = "[^0-9a-zA-Z]+";
+
+            //Create a replacement: replaces the entire match with the first captured subexpression.
+            string replacement = "$1";
+
+            //Invoke replace method on regex class to remove all non alphanumeric characters
+            return Regex.Replace(arg, pattern, replacement);
         }
     }
 }
