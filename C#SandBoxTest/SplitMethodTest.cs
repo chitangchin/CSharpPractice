ee168a7540f6fd9c909babfbd68aa2aa42ddb3fc
chitangchin <chitangb.chin@gmail.com>
2024-08-12 10:41:05 -0400

    Add project files.

---
+++ C#SandBoxTest/SplitMethodTest.cs
@@ -0,0 +1,15 @@
+﻿using Sandbox.SplitMethod;
+
+namespace C_SandBoxTest
+{
+    [TestFixture]
+    public class SplitMethodTests
+    {
+        [Test]
+        public void Tests()
+        {
+            string test = "Hello Test 1 2";
+            Assert.That(SolutionClass.SplitMethod(test), Is.EqualTo(test.Split(" ")));
+        }
+    }
+}
---
+++ C#Sandbox/SplitMethod.cs
@@ -0,0 +1,16 @@
+﻿//Testing if Split() method is the same output as Split(" ")
+namespace Sandbox.SplitMethod
+{
+    public class SolutionClass
+    {
+        public static string[] SplitMethod(string args)
+        {
+            return args.Split();
+        }
+    }
+
+}
+
+
+
+
